5f1fb695009c6374a3cfa16479f3a06d6a464e07
HEROEngineer <andylam20029@gmail.com>
2018-09-19 21:52:27 +0200

    added initialization scripts for drill, jupyter, ganglia and more goodies

--- dataproc/main.tf
+++ dataproc/main.tf
@@ -58,7 +58,75 @@ resource "google_dataproc_cluster" "poccluster" {
       script      = "gs://dataproc-initialization-actions/stackdriver/stackdriver.sh"
       timeout_sec = 500
     }
+
+    initialization_action {
+      script      = "gs://dataproc-initialization-actions/drill/drill.sh"
+      timeout_sec = 500
+    }
+
+    initialization_action {
+      script      = "gs://dataproc-initialization-actions/jupyter2/jupyter2.sh"
+      timeout_sec = 500
+    }
+
+    initialization_action {
+      script      = "gs://dataproc-initialization-actions/ganglia/ganglia.sh"
+      timeout_sec = 500
+    }
   }
 
   depends_on = ["google_storage_bucket.pocstagingbuck"]
+}
+
+# Submit an example spark job to a dataproc cluster
+resource "google_dataproc_job" "spark" {
+  region       = "${google_dataproc_cluster.poccluster.region}"
+  force_delete = true
+
+  placement {
+    cluster_name = "${google_dataproc_cluster.poccluster.name}"
+  }
+
+  spark_config {
+    main_class    = "org.apache.spark.examples.SparkPi"
+    jar_file_uris = ["file:///usr/lib/spark/examples/jars/spark-examples.jar"]
+    args          = ["1000"]
+
+    properties = {
+      "spark.logConf" = "true"
+    }
+
+    logging_config {
+      driver_log_levels {
+        "root" = "INFO"
+      }
+    }
+  }
+}
+
+# Submit an example pyspark job to a dataproc cluster
+resource "google_dataproc_job" "pyspark" {
+  region       = "${google_dataproc_cluster.poccluster.region}"
+  force_delete = true
+
+  placement {
+    cluster_name = "${google_dataproc_cluster.poccluster.name}"
+  }
+
+  pyspark_config {
+    main_python_file_uri = "gs://dataproc-examples-2f10d78d114f6aaec76462e3c310f31f/src/pyspark/hello-world/hello-world.py"
+
+    properties = {
+      "spark.logConf" = "true"
+    }
+  }
+}
+
+# Check out current state of the jobs
+output "spark_status" {
+  value = "${google_dataproc_job.spark.status.0.state}"
+}
+
+output "pyspark_status" {
+  value = "${google_dataproc_job.pyspark.status.0.state}"
 }
